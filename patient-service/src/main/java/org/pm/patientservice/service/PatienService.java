304883626f1a8c94f4ed666b8bc4fb1e0404278d
er-rajy <noureddineerajy@gmail.com>
2026-08-04 14:29:31 +0100

    Refactor PatienService to include PatienMapper and convert Patient list to PatienResponseDTO

--- patient-service/src/main/java/org/pm/patientservice/service/PatienService.java
+++ patient-service/src/main/java/org/pm/patientservice/service/PatienService.java
@@ -1,11 +1,14 @@
 package org.pm.patientservice.service;
 
 import org.pm.patientservice.dto.PatienResponseDTO;
+import org.pm.patientservice.mapper.PatienMapper;
 import org.pm.patientservice.model.Patient;
 import org.pm.patientservice.repository.PatienRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class PatienService {
     private PatienRepository patienRepository;
     public PatienService(PatienRepository patienRepository) {
@@ -13,7 +16,12 @@ public class PatienService {
     }
 
     public List<PatienResponseDTO> getPatiens(){
-        List<Patient> patients = patienRepository.findAll();
+
+        List<Patient> patients = patienRepository.findAll(); // get list of patiens as class
+//        List<PatienResponseDTO> patienResponseDTOS = patients.stream() // now mapped the lis of classes pateint to list of dto classes by streams.
+////                .map(patient -> PatienMapper.toDTO(patient)).toList(); normal logique
+//                .map(PatienMapper::toDTO).toList(); // by lamda methode reference
+        return patients.stream().map(PatienMapper::toDTO).toList();
     }
 
 }
